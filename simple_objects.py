b0d188e41be996a8b76d1304d202225c87c161be
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-16 16:15:44 +0200

    added attributes for company and model, added str method

--- simple_objects.py
+++ simple_objects.py
@@ -1,18 +1,25 @@
 class BasicAttributes:
     def __init__(
             self,
+            company: str,
+            model: str,
             color: str,
             year: int,
             cc: int,
             hp: int,
             total_km: float
     ) -> None:
-        
+
+        self.company = company
+        self.model = model
         self.color = color
         self.year = year
         self.cc = cc
         self.hp = hp
         self.total_km = total_km
+
+    def __str__(self) -> str:
+        return f""
 
 
 basic_chars = BasicAttributes(
@@ -22,3 +29,5 @@ basic_chars = BasicAttributes(
     hp = 68,
     total_km=365000
 )
+
+basic_chars.__str__()
