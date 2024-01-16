770957138d1a14b4b4d550986c746f259f9cf91c
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-16 16:12:59 +0200

    initialized object

--- simple_objects.py
+++ simple_objects.py
@@ -13,4 +13,12 @@ class BasicAttributes:
         self.cc = cc
         self.hp = hp
         self.total_km = total_km
-        
+
+
+basic_chars = BasicAttributes(
+    color="red",
+    year=1998,
+    cc = 1200,
+    hp = 68,
+    total_km=365000
+)
