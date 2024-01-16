a2882677ba8bdd73831df5d11dd5de3cf4475543
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-16 16:02:40 +0200

    added BasicAttributes class

---
+++ simple_objects.py
@@ -0,0 +1,9 @@
+class BasicAttributes:
+    def __init__(
+            self,
+            color: str,
+            year: int,
+            cc: int,
+            hp: int,
+            total_km: float
+    ) -> None:
