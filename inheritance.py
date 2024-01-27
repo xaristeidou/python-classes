c86b79596daad4d73035ba26da2c18e395a67e80
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-27 04:27:16 +0200

    simple inheritance example

--- inheritance.py
+++ inheritance.py
@@ -15,3 +15,21 @@ class Fruit:
     def fruit_print_details(self) -> None:
         return f"This is a {self.fruit_name}, is {self.color}, and weights {self.weight}"
     
+'''
+This is a Child class. It inherites that attributes and methods from the 'Fruit' class
+We can access all the attributes and methods that are assigned to the Parent class
+'''
+class Watermellon(Fruit):
+    pass
+
+watermellon = Watermellon(
+    fruit_name="watermellon",
+    color="red_green",
+    weight=10.5
+)
+
+watermellon.fruit_name
+watermellon.color
+watermellon.weight
+
+watermellon.fruit_print_details()
