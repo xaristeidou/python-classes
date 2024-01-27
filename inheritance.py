9c785ea65b6612217382c10280e9120a61763a48
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-27 04:20:37 +0200

    added Fruit parent class

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class Fruit:
+    def __init__(
+            self,
+            fruit_name: str,
+            color: str,
+            weight: float
+    ) -> None:
+        self.fruit_name = fruit_name
+        self.color = color
+        self.weight = weight
+
+    def fruit_print_details(self) -> None:
+        return f"This is a {self.fruit_name}, is {self.color}, and weights {self.weight}"
