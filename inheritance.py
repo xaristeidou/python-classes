3b52ca26b360faaad4d0626fa9b84e782d7a5b38
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-27 04:38:56 +0200

    added example of __init__() override

--- inheritance.py
+++ inheritance.py
@@ -33,3 +33,19 @@ watermellon.color
 watermellon.weight
 
 watermellon.fruit_print_details()
+
+
+
+'''
+We pass 'Fruit' class in the 'Apple' class, but this time we define the
+__init__() method. This will override the __init__() of the Parent class.
+Now if we try to access any attribute from 'Fruit' class we won't be able to.
+Nevertheless, all methods from Parent class are inhereted. 
+'''
+class Apple(Fruit):
+    def __init__(self) -> None:
+        pass 
+
+    
+apple = Apple()
+apple.fruit_print_details
