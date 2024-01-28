3c062047cf4112f94557b0c12b5080edc8d3e778
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-28 22:19:52 +0200

    added empty __init__() in child class example

--- inheritance.py
+++ inheritance.py
@@ -48,14 +48,19 @@ watermellon.fruit_print_details()
 We pass 'Fruit' class in the 'Apple' class, but this time we define the
 __init__() method. This will override the __init__() of the Parent class.
 Now if we try to access any attribute from 'Fruit' class we won't be able to.
-Nevertheless, all methods from Parent class are inhereted. 
+Nevertheless, all methods from Parent class are inhereted. If methods use
+attributes they won't be usable, but they are defined.
 '''
 class Apple(Fruit):
-    def __init__(self) -> None:
-        pass 
+    def __init__(self):
+        pass
 
-    
 apple = Apple()
+apple.fruit_name
+apple.color
+apple.weight
+apple.fruit_print_details
+
 apple.color # will raise an error
 apple.fruit_print_details # will show the method is constructed
 
