283f3521ca71e1c98c76582134892b458f91c964
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-02-07 21:48:26 +0200

    added details about raise error in final position of iterable

--- iterators.py
+++ iterators.py
@@ -2,6 +2,8 @@
 Iterators can be used to a lot of iterable objects
 Using iter() we create an iterator of an object
 The next() when used with an iterator will return the next value
+When the iterable object has reached the final position the next call of 
+next() will raise a StopIteration error
 '''
 
 example_tuple = (1,2,3)
